b24066b511c1f380c5a30b85a28e2e48389ec4a0
xavionashik <ashiksust26th@gmail.com>
2022-04-07 02:58:06 +0600

    added another function

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import "./Cart.css";
 
 const Cart = ({cart}) => {
+
+    let total = 0;
+    let shipping = 0;
+    for ( const product of cart) {
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+
+    let tax = parseFloat((total * 0.1).toFixed(2));
+
+    const grandTotal = total + shipping + tax;
+
     return (
-        <div>
-            <h4>this is order summary</h4>  
-            <p>selected items : {cart.length}</p>
+        <div className='cart'>
+            <h3>Order Summary</h3>  
+            <p>Selected items : {cart.length}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping Charge: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h4>Grand Total: ${grandTotal}</h4>
         </div>
     );
 };
