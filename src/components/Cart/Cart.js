e8468f9e20611f9e09cd9bf21c0967d0d2dd4ec1
xavionashik <ashiksust26th@gmail.com>
2022-04-07 13:10:09 +0600

    cart calculation with quantity inlcuded

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,21 +3,22 @@ import "./Cart.css";
 
 const Cart = ({cart}) => {
 
+
     let total = 0;
     let shipping = 0;
-    for ( const product of cart) {
-        total = total + product.price;
+    let quantity = 0;
+    for(const product of cart){
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
         shipping = shipping + product.shipping;
     }
-
-    let tax = parseFloat((total * 0.1).toFixed(2));
-
+    const tax = parseFloat((total * 0.1).toFixed(2));
     const grandTotal = total + shipping + tax;
 
     return (
         <div className='cart'>
             <h3>Order Summary</h3>  
-            <p>Selected items : {cart.length}</p>
+            <p>Selected items : {quantity}</p>
             <p>Total Price: ${total}</p>
             <p>Total Shipping Charge: ${shipping}</p>
             <p>Tax: ${tax}</p>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -34,16 +34,15 @@ const Shop = () => {
     }, [products]);
 
     const handleAddToCart = (selectedProduct) => {
-        console.log(selectedProduct)
         let newCart = [];
-        const exists = cart.find(product => product.id === selectedProduct.id);
+        let exists = cart.find(product => product.id === selectedProduct.id);
         if (!exists) {
             selectedProduct.quantity = 1;
             newCart = [...cart, selectedProduct];
         } 
         else {
             const rest = cart.filter(product => product.id !== selectedProduct.id);
-            exists.quantity = exists.quantity + 1; 
+            exists.quantity = exists.quantity + 1;
             newCart = [...rest, exists];
         }
         setCart(newCart);
